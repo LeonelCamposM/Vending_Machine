534ec32904e30093a767356c7b26ee7f506e0d76
LeonelCamposM <89167230+LeonelCamposM@users.noreply.github.com>
2022-07-23 19:05:40 -0600

    add application layers (#1)

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Application.Persons;
+using Application.Persons.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IPersonService, PersonService>();
+            return services;
+        }
+    }
+}
