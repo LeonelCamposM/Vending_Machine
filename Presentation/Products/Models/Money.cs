7f56fee0b14adcebf3a3309228ad8522bdcd20ef
LeonelCamposM <leonelscamposm@gmail.com>
2022-07-25 15:09:27 -0600

    add payment view AC#1 AC#2

---
+++ Presentation/Products/Models/Money.cs
@@ -0,0 +1,16 @@
+﻿namespace Presentation.Products.Models
+{
+    internal class Money
+    {
+        public string type { get; set; }
+        public double value { get; set; }
+        public int enteredUnits { get; set; }
+
+        public Money(string type, double value, int enteredUnits)
+        {
+            this.type = type;   
+            this.value = value;
+            this.enteredUnits = enteredUnits;
+        }
+    }
+}
